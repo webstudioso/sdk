a006fa62ad3eabcaa348a5c5e9e500508ca99bc2
Giancarlo Sanchez <gianksp@gmail.com>
2023-01-12 12:03:55 +0000

    Removing etherjs from bundle

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { ethers } from "ethers";
 import * as UAuthWeb3Modal from "@uauth/web3modal";
 import UAuthSPA from "@uauth/js";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3Modal from "web3modal";
 
+
 window.dappify = {
 	ThirdwebSDK,
-	ethers,
 	UAuthWeb3Modal,
 	UAuthSPA,
 	WalletConnectProvider,
-	Web3Modal
+	Web3Modal,
 }
+
 console.log("Dappify bundle loaded...") 
 console.log(dappify);
